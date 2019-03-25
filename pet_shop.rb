a4f9cdc3fdff40f92d836df3d867e3db5bdc455c
dataSlayer64275 <ess.knst@gmail.com>
2019-03-25 09:32:23 +0000

    Initial Commit

---
+++ pet_shop.rb
@@ -0,0 +1,91 @@
+def pet_shop_name(petshop)
+  return petshop[:name]
+end
+
+def total_cash (petshop)
+  return petshop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(petshop, money)
+  new_total = petshop[:admin][:total_cash] += money
+  return new_total
+end
+
+def pets_sold(petshop)
+  return petshop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(petshop, new_pet_sales)
+  return petshop[:admin][:pets_sold] += new_pet_sales
+end
+
+def stock_count(petshop)
+  return petshop[:pets].length
+end
+
+def pets_by_breed(petshop, dog_breed)
+pets = []
+for pet in petshop[:pets]
+  if pet[:breed] == dog_breed
+    pets.push(pet[:breed])
+  else
+  end
+end
+return pets
+end
+
+def find_pet_by_name(petshop,name)
+  for element in petshop[:pets]
+    if element[:name] == name
+      return element
+    else
+    end
+  end
+  return nil
+end
+
+def remove_pet_by_name(petshop,name)
+  for element in petshop[:pets]
+    if element[:name] == name
+      element.clear
+    else
+    end
+  end
+  return element
+end
+
+def add_pet_to_stock(petshop, newpet)
+  petshop[:pets][6] = newpet
+  return petshop[:pets].length
+end
+
+def customer_cash(customer_position)
+  return customer_position[:cash]
+end
+
+def remove_customer_cash(customer_position, amount)
+  customer_position[:cash] -= amount
+  return customer_position[:cash]
+end
+
+def customer_pet_count(customer_position)
+  return customer_position[:pets].length
+end
+
+def add_pet_to_customer(customer, newpet)
+  customer[:pets][0] = newpet
+  return customer[:pets]
+end
+
+#OPTIONAL
+
+def customer_can_afford_pet(customer, newpet)
+  if customer[:cash] >= newpet[:price]
+    then
+    return true
+  else
+    return false
+  end
+end
+
+# def sell_pet_to_customer(petshop, name)
